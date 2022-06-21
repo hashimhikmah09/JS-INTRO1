bc2c8251f8d74eba24f53dec74d66ba81e949f98
Hikmah <hashimhikmah09@gmail.com>
2022-06-21 12:47:52 +0100

    first javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+let firstName = 'Hikmah'
+document.write( "My name is " + firstName + "<br>"); 
+let height = 1.8
+document.write( "I am" + height + "feet tall <br>");
+let country = "Nigeria"
+document.write( "My country is" + country);
